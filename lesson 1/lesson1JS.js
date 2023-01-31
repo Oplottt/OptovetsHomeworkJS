6709306fe9a916fd4b749d4ad53946b52c8a1776
Oplottt <bogdan.abrams@gmail.com>
2023-01-31 22:55:15 +0200

    дз 31.01

---
+++ lesson 1/lesson1JS.js
@@ -0,0 +1,37 @@
+let a = "hello";
+console.log(a);
+let b = "owu";
+console.log(b);
+let c = "com";
+console.log(c);
+let d = "ua";
+console.log(d);
+let num1 = 1;
+console.log(num1);
+let num2 = 10;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+let num4 = 123;
+console.log(num4);
+let num5 = 3.14;
+console.log(num5);
+let num6 = 2.7;
+console.log(num6);
+let num7 = 16;
+console.log(num7);
+let bool1 = true;
+console.log(bool1);
+let bool2 = false;
+console.log(bool2)
+let firstName = ("B");
+let secondName = ("O");
+let lastName = ("V");
+let msg = firstName + ' ' + secondName + ' ' + lastName;
+console.log(msg)
+let aa = 100;
+console.log(typeof aa);
+let bb = '100';
+console.log(typeof bb);
+let cc = true;
+console.log(typeof cc);
